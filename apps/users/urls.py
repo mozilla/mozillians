1e9b5a83aacf5894521d5fe91ee94382f44d1964
Austin King <shout@ozten.com>
2011-08-04 16:25:03 -0700

    Sigh. Missed commit.

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -12,7 +12,7 @@ auth_views.render_to_response = jinja_for_django
 
 urlpatterns = patterns('',
     url(r'^login', auth_views.login, name='login'),
-    url(r'^logout', auth_views.logout, dict(redirect_field_name=next), 
+    url(r'^logout', auth_views.logout, dict(redirect_field_name='next'), 
         name='logout'),
     url(r'^register', views.register, name='register'),
 )
