2888f81eb7461e099f6da682969eee3091fc9865
Matthew Riley MacPherson <mmacpherson@mozilla.com>
2011-08-30 21:57:38 -0300

    Remove redundant lambdas from urls.py

--- urls.py
+++ urls.py
@@ -19,7 +19,7 @@ def _error_page(request, status):
 
 handler404 = lambda r: _error_page(r, 404)
 handler500 = lambda r: _error_page(r, 500)
-handler_csrf = lambda r: jingo.render(r, 'csrf_error.html', status=400)
+handler_csrf = lambda r, cb=None: jingo.render(r, 'csrf_error.html', status=400)
 
 
 urlpatterns = patterns('',
@@ -39,7 +39,7 @@ if settings.DEBUG:
         (r'^%s/(?P<path>.*)$' % media_url, 'django.views.static.serve',
          {'document_root': settings.MEDIA_ROOT}),
         # Add the 404, 500, and csrf pages for testing
-        (r'^404$', lambda r: _error_page(r, 404)),
-        (r'^500$', lambda r: _error_page(r, 500)),
-        (r'^csrf$', lambda r: jingo.render(r, 'csrf_error.html', status=400)),
+        (r'^404$', handler404),
+        (r'^500$', handler500),
+        (r'^csrf$', handler_csrf),
     )
