86b6e72599c53e03a0c17bca5fa247103d333cb8
Dave Dash <dd+github@davedash.com>
2011-10-03 09:57:30 -0700

    Adds French and Polish to production, fix bug 691359

--- settings/default.py
+++ settings/default.py
@@ -23,7 +23,7 @@ LOCALE_PATHS = [path('locale')]
 
 
 # Accepted locales
-PROD_LANGUAGES = ('de', 'en-US', 'es', 'sl', 'sq', 'zh-TW')
+PROD_LANGUAGES = ('de', 'en-US', 'es', 'fr', 'pl', 'sl', 'sq', 'zh-TW')
 
 # List of RTL locales known to this project. Subset of LANGUAGES.
 RTL_LANGUAGES = ()  # ('ar', 'fa', 'fa-IR', 'he')
