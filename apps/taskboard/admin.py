6f385a60e0a540b0c985fe9edfb600a446bceee3
Paul McLanahan <pmclanahan@mozilla.com>
2012-01-18 15:34:27 -0500

    Add disable and enable actions to tasks admin. Re #92.

--- apps/taskboard/admin.py
+++ apps/taskboard/admin.py
@@ -2,7 +2,18 @@ from django.contrib import admin
 from taskboard.models import Task
 
 
+def mark_disabled(modeladmin, request, queryset):
+    queryset.update(disabled=True)
+mark_disabled.short_description = "Disable selected tasks"
+
+
+def mark_enabled(modeladmin, request, queryset):
+    queryset.update(disabled=False)
+mark_enabled.short_description = "Enable selected tasks"
+
+
 class TaskAdmin(admin.ModelAdmin):
+    actions = (mark_disabled, mark_enabled)
     list_display = ('summary', 'contact', 'deadline', 'created', 'disabled')
     list_filter = ('deadline', 'created', 'disabled')
     ordering = ('-created',)
