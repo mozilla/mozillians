53226ef51328376b6c72e2ba12a6370a2afb68b5
Giorgos Logiotatidis <giorgos@mozilla.com>
2012-11-14 14:51:07 +0200

    [fix bug 790171] Add japanise translation.

--- settings/default.py
+++ settings/default.py
@@ -24,7 +24,7 @@ LOCALE_PATHS = [path('locale')]
 
 # Accepted locales
 PROD_LANGUAGES = ('ca', 'cs', 'de', 'en-US', 'es', 'hu', 'fr', 'ko', 'nl',
-                  'pl', 'pt-BR', 'ru', 'sk', 'sl', 'sq', 'zh-TW', 'lt')
+                  'pl', 'pt-BR', 'ru', 'sk', 'sl', 'sq', 'zh-TW', 'lt', 'ja')
 
 # List of RTL locales known to this project. Subset of LANGUAGES.
 RTL_LANGUAGES = ()  # ('ar', 'fa', 'fa-IR', 'he')
