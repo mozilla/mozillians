a7c9c06f8546663f7e06ca1b287f011ff24f56e1
Giorgos Logiotatidis <giorgos@mozilla.com>
2012-12-11 19:09:06 +0200

    [fix bug 820747] [fix bug 820398] Enhance 'Users' and 'Groups' Admin panel.

--- apps/groups/admin.py
+++ apps/groups/admin.py
@@ -1,11 +1,35 @@
 from django.contrib import admin
+from django.contrib.admin import SimpleListFilter
 
 from models import Group
 
 
+class CurratedGroupFilter(SimpleListFilter):
+    """Admin filter for curated groups."""
+    title = 'curated'
+    parameter_name = 'curated'
+
+    def lookups(self, request, model_admin):
+        return (('False', 'Curated'),
+                ('True', 'Not curated'))
+
+    def queryset(self, request, queryset):
+        if self.value() is None:
+            return queryset
+        value = self.value() == 'True'
+        return queryset.filter(steward__isnull=value)
+
+
 class GroupAdmin(admin.ModelAdmin):
-    list_display = ['name', 'steward', 'wiki', 'website', 'irc_channel']
+    """Group Admin."""
+    list_display = ['name', 'steward', 'wiki', 'website', 'irc_channel',
+                    'no_members']
     search_fields = ['name']
     raw_id_fields = ['steward']
+    list_filter = [CurratedGroupFilter]
+
+    def no_members(self, obj):
+        """Return number of members in group."""
+        return obj.userprofile_set.count()
 
 admin.site.register(Group, GroupAdmin)
