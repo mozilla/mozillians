9cb0a324f910dfdd1daf54063122bd2aba008b37
Giorgos Logiotatidis <giorgos@mozilla.com>
2012-12-17 11:41:34 +0200

    Update (django-)celery.

---
+++ scripts/drop_celery_tables.sql
@@ -0,0 +1,6 @@
+DROP TABLE djcelery_taskstate;
+DROP TABLE djcelery_workerstate;
+DROP TABLE djcelery_periodictasks;
+DROP TABLE djcelery_periodictask;
+DROP TABLE djcelery_intervalschedule;
+DROP TABLE djcelery_crontabschedule;
