046a29da38d22bec5b6ac4fb418635bf9a8e2c76
Giorgos Logiotatidis <giorgos@mozilla.com>
2013-02-13 12:46:54 +0200

    Remove unused urls.

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -12,17 +12,8 @@ auth_views.render_to_response = jinja_for_django
 
 urlpatterns = patterns('',
     url(r'^logout$', views.logout, name='logout'),
-    url(r'^confirm$', redirect_to, dict(url='/', name='home')),
     url('^browserid/verify/', views.BrowserID.as_view(),
                               name='browserid_verify'),
 
-    # This sucks: we should not have to do this, but a lot of people/libraries/
-    # existing code is looking for this view.
-    url(r'^login$', redirect_to, dict(url='/', name='home'), name='login'),
-    url(r'^register$', views.register, name='register'),
-    # TODO: remove in 1.4 release, legacy url sent in emails
-    url(r'^password_reset_confirm/'
-         '(?P<uidb36>[0-9A-Za-z]{1,13})-(?P<token>[0-9A-Za-z]{1,13}-'
-         '[0-9A-Za-z]{1,20})/$',
-        redirect_to, dict(url='/', name='password_reset_confirm')),
+    url(r'^register$', views.register, name='register')
 )
