6f42186c3496c01e0c4dbe80b30bc2e1bb08edfa
Michał Frontczak <michaljev@o2.pl>
2013-05-14 19:02:52 +0200

    [fix bug 757953] Make settings have saner defaults for database

--- settings/default.py
+++ settings/default.py
@@ -22,6 +22,22 @@ LOGGING = {
     },
 }
 
+## Database settings
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '',
+        'PORT': '',
+        'OPTIONS': {
+            'init_command': 'SET storage_engine=InnoDB',
+            'charset': 'utf8',
+            'use_unicode': True,
+        },
+        'TEST_CHARSET': 'utf8',
+        'TEST_COLLATION': 'utf8_general_ci',
+    },
+}
+						
 ## L10n
 LOCALE_PATHS = [path('locale')]
 
