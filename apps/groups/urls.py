4a1a5e3825dc4f0a0382a493c00b69add1d0fb6f
Michał Frontczak <michaljev@o2.pl>
2013-06-11 14:33:35 +0200

    [fix bug 845793] Add "Browse Functional Areas" view

--- apps/groups/urls.py
+++ apps/groups/urls.py
@@ -6,6 +6,8 @@ from models import Group, Skill, Language
 urlpatterns = patterns('',
     url('^groups/$', views.index, name='group_index'),
     url('^group/(?P<url>[-\w]+)/$', views.show, name='group'),
+    url('^functional-areas/$', views.index_functional_areas,
+        name='functional_areas_index'), 
     url('^group/(?P<url>[-\w]+)/toggle/$', views.toggle, name='group_toggle'),
     url('^groups/search/$', views.search,
         dict(searched_object=Group), name='group_search'),
