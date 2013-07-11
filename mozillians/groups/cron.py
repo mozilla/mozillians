4a031642ec93cc2a771de7351ec9c7a5a72eef86
Giorgos Logiotatidis <giorgos@mozilla.com>
2013-07-11 11:13:08 +0300

    Remove assign_staff_to_early_users deprecated function.

--- mozillians/groups/cron.py
+++ mozillians/groups/cron.py
@@ -35,19 +35,3 @@ def assign_autocomplete_to_groups():
                               .annotate(count=Count('members'))):
         g.auto_complete = g.count > AUTO_COMPLETE_COUNT
         g.save()
-
-
-@cronjobs.register
-def assign_staff_to_early_users():
-    """Add 'staff' group to all auto-vouched users."""
-    staff = Group.objects.get(name='staff')
-    staff_users = []
-
-    for d in settings.AUTO_VOUCH_DOMAINS:
-        if not staff_users:
-            staff_users = User.objects.filter(email__iendswith=d)
-        else:
-            staff_users = staff_users | staff_users.filter(email__iendswith=d)
-
-    for u in staff_users:
-        u.get_profile().groups.add(staff)
