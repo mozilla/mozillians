a814dc7f4b5a7157d6514ee0ced0e2e0f5fb63d9
Giorgos Logiotatidis <giorgos@mozilla.com>
2013-09-02 13:54:39 +0300

    [fix bug 911980][fix bug 848256][fix bug 848257] API refactoring, tests and bug fixing.
    * Refactor API to not use the ElasticSearch backend (bug 911980)
    * Explicitly set the fields returned by UserResource
    * Fix filters containing spaces (bug 848257)
    * Fix filters with commas (bug 848256)
    * Fix default data ordering (bug 914384)

--- mozillians/users/api.py
+++ mozillians/users/api.py
@@ -1,9 +1,9 @@
 from urllib2 import unquote
 from urlparse import urljoin
 
+from django.db.models import Q
 from django.conf import settings
 
-from elasticutils.contrib.django import F, S
 from tastypie import fields
 from tastypie import http
 from tastypie.bundle import Bundle
@@ -21,6 +21,8 @@ from mozillians.users.models import UserProfile
 class UserResource(ClientCachedResource, ModelResource):
     """User Resource."""
     email = fields.CharField(attribute='user__email', null=True, readonly=True)
+    vouched_by = fields.IntegerField(attribute='vouched_by__id',
+                                     null=True, readonly=True)
     groups = fields.CharField()
     skills = fields.CharField()
     languages = fields.CharField()
@@ -29,7 +31,7 @@ class UserResource(ClientCachedResource, ModelResource):
         queryset = UserProfile.objects.all()
         authentication = AppAuthentication()
         authorization = MozillaOfficialAuthorization()
-        serializer = Serializer(formats=['json', 'jsonp', 'xml'])
+        serializer = Serializer(formats=['json', 'jsonp'])
         paginator_class = Paginator
         cache_control = {'max-age': 0}
         list_allowed_methods = ['get']
@@ -37,16 +39,51 @@ class UserResource(ClientCachedResource, ModelResource):
         resource_name = 'users'
         restrict_fields = False
         restricted_fields = ['email', 'is_vouched']
-        fields = []
+        fields = ['id', 'full_name', 'is_vouched', 'website', 'vouched_by',
+                  'date_vouched', 'groups', 'skills', 'languages',
+                  'bio', 'photo', 'ircname', 'country', 'region', 'city',
+                  'date_mozillian', 'timezone', 'tshirt', 'email',
+                  'allows_mozilla_sites', 'allows_community_sites']
 
     def build_filters(self, filters=None):
-        es_filters = []
-        for item in set(['email', 'country', 'region', 'city', 'ircname',
-                         'username', 'languages', 'skills', 'groups',
-                         'is_vouched', 'name']) & set(filters):
-            es_filters.append(F(**{item: unquote(filters[item]).lower()}))
+        database_filters = {}
+        valid_filters =  [f for f in filters if f in
+                          ['email', 'country', 'region', 'city', 'ircname',
+                           'username', 'groups', 'languages', 'skills',
+                           'is_vouched', 'name']]
+        getvalue = lambda x: unquote(filters[x].lower())
 
-        return es_filters
+        if 'email' in valid_filters:
+            database_filters['email'] = Q(
+                user__email__iexact=getvalue('email'))
+
+        if 'username' in valid_filters:
+            database_filters['username'] = Q(
+                user__username__iexact=getvalue('username'))
+
+        if 'name' in valid_filters:
+            database_filters['name'] = Q(full_name__iexact=getvalue('name'))
+
+        if 'is_vouched' in valid_filters:
+            value = getvalue('is_vouched')
+            if value == 'true':
+                database_filters['is_vouched'] = Q(is_vouched=True)
+            elif value == 'false':
+                database_filters['is_vouched'] = Q(is_vouched=False)
+
+        for possible_filter in ['country', 'region', 'city', 'ircname']:
+            if possible_filter in valid_filters:
+                database_filters[possible_filter] = Q(
+                    **{'{0}__iexact'.format(possible_filter):
+                       getvalue(possible_filter)})
+
+        for group_filter in ['groups', 'languages', 'skills']:
+            if group_filter in valid_filters:
+                database_filters[group_filter] = Q(
+                    **{'{0}__name__in'.format(group_filter):
+                       getvalue(group_filter).split(',')})
+
+        return database_filters
 
     def dehydrate(self, bundle):
         if (bundle.request.GET.get('restricted', False)
@@ -55,17 +92,16 @@ class UserResource(ClientCachedResource, ModelResource):
             for key in self._meta.restricted_fields:
                 data[key] = bundle.data[key]
             bundle = Bundle(obj=bundle.obj, data=data, request=bundle.request)
-
         return bundle
 
     def dehydrate_groups(self, bundle):
-        return [unicode(g) for g in bundle.obj.groups.all()]
+        return bundle.obj.groups.values_list('name')
 
     def dehydrate_skills(self, bundle):
-        return [unicode(g) for g in bundle.obj.skills.all()]
+        return bundle.obj.skills.values_list('name')
 
     def dehydrate_languages(self, bundle):
-        return [unicode(g) for g in bundle.obj.languages.all()]
+        return bundle.obj.languages.values_list('name')
 
     def dehydrate_photo(self, bundle):
         if bundle.obj.photo:
@@ -79,23 +115,16 @@ class UserResource(ClientCachedResource, ModelResource):
         return super(UserResource, self).get_detail(request, **kwargs)
 
     def apply_filters(self, request, applicable_filters):
-        """Implement advanced filters.
-
-        - Implement 'groups' filter.
-        - Implement 'languages' filter.
-        - Implement 'skills' filter.
-
-        """
         if (request.GET.get('restricted', False)
-            and 'email__text' not in applicable_filters
+            and 'email' not in applicable_filters
             and len(applicable_filters) != 1):
             raise ImmediateHttpResponse(response=http.HttpForbidden())
 
+        mega_filter = Q()
+        for db_filter in applicable_filters.values():
+            mega_filter &= db_filter
+
         if request.GET.get('restricted', False):
-            applicable_filters.append(F(allows_community_sites=True))
+            mega_filter &= Q(allows_community_sites=True)
 
-        mega_filter = F()
-        for filter in applicable_filters:
-            mega_filter &= filter
-
-        return S(UserProfile).filter(mega_filter)
+        return UserProfile.objects.filter(mega_filter).order_by('id')
