0525b420e13cf2e4e8ed50aa218e67fb18279f8f
Dan Poirier <dpoirier@caktusgroup.com>
2013-11-06 13:34:37 -0500

    Bug 877315 - count vouched members in admin
    In the admin list for groups, add a column to display the number of
    vouched members, similar to the current column that counts all
    members.
    
    This implementation relies on the database implementing Booleans
    as integers, so we can Sum() them. It works for MySQL, which is
    how Mozillians is currently deployed.
    
    A test is added that will fail if we ever try to use Mozillians
    with a database where this doesn't work, which will alert us to
    find another approach.

---
+++ mozillians/groups/tests/test_admin.py
@@ -0,0 +1,40 @@
+from django.contrib.admin.sites import site
+from django.http import HttpRequest
+
+from mock import Mock
+from nose.tools import eq_, ok_
+
+from mozillians.common.tests import TestCase
+from mozillians.groups.admin import GroupAdmin
+from mozillians.groups.models import Group
+from mozillians.groups.tests import GroupFactory
+from mozillians.users.tests import UserFactory
+
+
+class TestGroupAdmin(TestCase):
+    def test_member_counts(self):
+        # The Group admin computes how many vouched members there are
+        # and how many overall
+
+        # IMPORTANT: This test is expected to fail on Postgres, and
+        # probably other databases where the Boolean type is not just
+        # an alias for a small integer. Mozillians is currently
+        # deployed on a database where this works. If we ever try
+        # deploying it on another database where it doesn't work, this
+        # test will alert us quickly that we'll need to take another
+        # approach to this feature.
+
+        # Create group with 1 vouched member and 1 unvouched member
+        group = GroupFactory()
+        user = UserFactory(userprofile={'is_vouched': False})
+        group.members.add(user.userprofile)
+        user2 = UserFactory(userprofile={'is_vouched': True})
+        group.members.add(user2.userprofile)
+
+        admin = GroupAdmin(model=Group, admin_site=site)
+        mock_request = Mock(spec=HttpRequest)
+        qset = admin.queryset(mock_request)
+
+        g = qset.get(name=group.name)
+        eq_(2, g.member_count)
+        eq_(1, g.vouched_member_count)
