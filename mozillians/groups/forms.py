dd359b43eb5cadb42b36a39d2657b507a1c47eab
Dan Poirier <dpoirier@caktusgroup.com>
2013-11-26 08:27:23 -0500

    Remove some unused code from groups/forms.py

--- mozillians/groups/forms.py
+++ mozillians/groups/forms.py
@@ -1,13 +1,6 @@
-import re
-
 from django import forms
-from django.core.exceptions import ValidationError
 
-from tower import ugettext as _
 from tower import ugettext_lazy as _lazy
-
-from mozillians.groups.helpers import stringify_groups
-from mozillians.groups.models import Group
 
 
 class SortForm(forms.Form):
@@ -21,39 +14,3 @@ class SortForm(forms.Form):
         if self.cleaned_data['sort'] == '':
             return 'name'
         return self.cleaned_data['sort']
-
-
-class GroupWidget(forms.TextInput):
-
-    def render(self, name, value, attrs=None):
-        if not (value is None or isinstance(value, basestring)):
-            value = stringify_groups(Group.objects.get(pk=v) for v in value)
-
-        return super(GroupWidget, self).render(name, value, attrs)
-
-
-class GroupField(forms.CharField):
-    widget = GroupWidget
-
-    def clean(self, value):
-        """Groups are saved in lowercase because it's easy and
-        consistent.
-
-        """
-        value = super(GroupField, self).clean(value)
-
-        if not re.match(r'^[a-zA-Z0-9 .:,-]*$', value):
-            raise ValidationError(_(u'Groups can only contain alphanumeric '
-                                    'characters, dashes, spaces.'))
-
-        values = [g.strip() for g in value.lower().split(',')
-                  if g and ',' not in g]
-
-        groups = []
-        for g in values:
-            (group, created) = Group.objects.get_or_create(name=g)
-
-            if not group.system:
-                groups.append(group)
-
-        return groups
