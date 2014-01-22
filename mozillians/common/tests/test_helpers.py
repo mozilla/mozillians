c76d25c2b05261661de4ff43d15b9a79d3c3aa44
Andrei Hajdukewycz <sancus@off.net>
2014-01-22 07:36:46 -0500

    [Bug 938005] Fix timezone helper tests

--- mozillians/common/tests/test_helpers.py
+++ mozillians/common/tests/test_helpers.py
@@ -1,11 +1,14 @@
 from django.template import Context
 from django.test.utils import override_settings
+from django.utils.timezone import is_aware
 
 from bleach import clean
+from datetime import datetime
 from jingo import env
 from markdown import markdown
 from mock import patch
 from nose.tools import eq_, ok_
+from pytz import utc
 
 from mozillians.common import helpers
 from mozillians.common.tests import TestCase
@@ -47,3 +50,37 @@ class HelperTests(TestCase):
         c = Context({'testkey': 'testvalue'})
         s = t.render(c)
         eq_('STARTEND', s)
+
+
+class TimezoneHelpers(TestCase):
+    @override_settings(USE_TZ=False)
+    def test_aware_now_if_use_tz_false(self):
+        # aware_utcnow returns an aware time, even if USE_TZ is False
+        ok_(is_aware(helpers.aware_utcnow()))
+
+    @override_settings(USE_TZ=True)
+    def test_aware_now_if_use_tz_true(self):
+        # aware_utcnow returns an aware time, even if USE_TZ is True
+        ok_(is_aware(helpers.aware_utcnow()))
+
+    def test_now_in_timezone(self):
+        # now_in_timezone returns the current time, expressed in the desired timezone
+        # Construct a time in UTC that will be "now"
+        utc_time = datetime(1972, 1, 1, 12, 4, 5).replace(tzinfo=utc)
+        tz_name = "US/Eastern"  # 5 hours difference from UTC on 1/1/1972
+        with patch('mozillians.common.helpers.aware_utcnow') as mock_aware_now:
+            mock_aware_now.return_value = utc_time
+            result = helpers.now_in_timezone(tz_name)
+        ok_(is_aware(result))
+        fmt_time = result.strftime("%H:%M %Z")
+        eq_('07:04 EST', fmt_time)
+
+    def test_offset_of_timezone(self):
+        # offset_of_timezone returns the offset in minutes of the named timezone as of now
+        # Construct a time in UTC that will be "now"
+        utc_time = datetime(1972, 1, 1, 3, 4, 5).replace(tzinfo=utc)
+        tz_name = "US/Eastern"  # 5 hours difference from UTC on 1/1/1972
+        with patch('mozillians.common.helpers.aware_utcnow') as mock_aware_now:
+            mock_aware_now.return_value = utc_time
+            result = helpers.offset_of_timezone(tz_name)
+        eq_(-300, result)
