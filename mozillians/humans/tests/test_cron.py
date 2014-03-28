3ffca093a9261055ac6a642b34e93fa7ba10c799
Giorgos Logiotatidis <giorgos@mozilla.com>
2014-03-28 16:45:53 +0200

    Update mozillians.human tests to not make actual calls to GitHub or SVN.
    And make them stronger tests too.

--- mozillians/humans/tests/test_cron.py
+++ mozillians/humans/tests/test_cron.py
@@ -1,16 +1,27 @@
-import os
-
 from django.conf import settings
 
-from nose.tools import ok_
+from mock import ANY, mock_open, patch
 
 from mozillians.common.tests import TestCase
 from mozillians.humans.cron import generate_humanstxt
 
 
 class TestCron(TestCase):
-    def test_generate(self):
-        if os.path.exists(settings.HUMANSTXT_FILE):
-            os.remove(settings.HUMANSTXT_FILE)
-        generate_humanstxt()
-        ok_(os.path.exists(settings.HUMANSTXT_FILE))
+    @patch('mozillians.humans.cron._get_githubbers')
+    @patch('mozillians.humans.cron._get_localizers')
+    @patch('mozillians.humans.cron.render_to_string')
+    def test_generate(self, render_to_string_mock, localizers_mock, githubbers_mock):
+        render_to_string_mock.return_value = 'rendered'
+        githubbers_mock.return_value = ['foo']
+        localizers_mock.return_value = ['bar']
+        open_mock = mock_open()
+        with patch('mozillians.humans.cron.open', open_mock, create=True):
+            generate_humanstxt()
+        data = {
+            'githubbers': ['foo'],
+            'localizers': ['bar'],
+            'last_update': ANY
+        }
+        render_to_string_mock.assert_called_with('humans/humans.txt', data)
+        open_mock.assert_called_with(settings.HUMANSTXT_FILE, 'w')
+        open_mock().write.assert_called_with('rendered')
