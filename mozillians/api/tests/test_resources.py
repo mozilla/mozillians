01dad238cb87841043e7db2706428097fd5cb194
Giorgos Logiotatidis <giorgos@mozilla.com>
2014-04-02 13:27:50 +0300

    [bug 978694] Tests for GraphiteMixin.
    This wasn't added to the original commit, too many untracked files.

---
+++ mozillians/api/tests/test_resources.py
@@ -0,0 +1,29 @@
+from mock import MagicMock, patch
+from nose.tools import eq_
+
+from mozillians.api.resources import GraphiteMixIn
+from mozillians.common.tests import TestCase
+
+
+class GraphiteMixInTests(TestCase):
+    @patch('mozillians.api.resources.statsd.incr')
+    def test_statsd_call(self, incr_mock):
+        real_wrapper = MagicMock()
+        view = MagicMock()
+
+        class Bar(object):
+            def wrap_view(self, view):
+                return real_wrapper
+
+        class Foo(GraphiteMixIn, Bar):
+            def __init__(self):
+                self.foo = view
+                self.foo.im_class.__name__ = 'foo'
+                self.foo.im_func.__name__ = 'bar'
+
+        foo = Foo()
+        return_value = foo.wrap_view('foo')('request', 1, second=2)
+
+        eq_(return_value, real_wrapper.return_value)
+        real_wrapper.assert_called_with('request', 1, second=2)
+        incr_mock.assert_called_with('api.resources.foo.bar')
