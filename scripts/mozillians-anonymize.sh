6c8b0ec997459c5e339b87422e34adeaf5ae6fbe
Sheeri <awfief@gmail.com>
2014-04-07 14:01:28 -0400

    Update mozillians-anonymize.sh
    copy dev over

--- scripts/mozillians-anonymize.sh
+++ scripts/mozillians-anonymize.sh
@@ -46,6 +46,12 @@ $MYSQL $STAGEDB < $SQLPATH/$DB.$TODAY.queries_sanitize_stage.sql
 /usr/bin/mysqldump $STAGEDB -u root $P -S /var/lib/mysql/$INSTANCE.sock > $SQLPATH/$DB.$TODAY.sanitized_stage.sql
 
 # copy dbs
-/usr/bin/scp $SQLPATH/$DB.$TODAY.sanitized_dev.sql $SQLPATH/$DB.$TODAY.sanitized_stage.sql  genericadm.private.phx1.mozilla.com:/data/genericrhel6-dev/src/mozillians-dev.allizom.org/mozillians/media/
-                
+# copy dbs; compress for the copy only
+gzip $SQLPATH/$DB.$TODAY.sanitized_dev.sql $SQLPATH/$DB.$TODAY.sanitized_stage.sql
+/usr/bin/scp $SQLPATH/$DB.$TODAY.sanitized_dev.sql.gz genericadm.private.phx1.mozilla.com:/data/genericrhel6-dev/src/mozillians-dev.allizom.org/mozillians/media/mozillians_org.latest.sanitized_dev.sql.gz
+# don't copy stage yet
+#/usr/bin/scp $SQLPATH/$DB.$TODAY.sanitized_stage.sql.gz genericadm.private.phx1.mozilla.com:/data/genericrhel6-dev/src/mozillians-dev.allizom.org/mozillians/media/mozillians_org.latest.sanitized_stage.sql.gz
+# leave this uncompressed for more efficient storage on our backup device (yes, really)
+gunzip $SQLPATH/$DB.$TODAY.sanitized_dev.sql.gz $SQLPATH/$DB.$TODAY.sanitized_stage.sql.gz            
+
 # Done!
