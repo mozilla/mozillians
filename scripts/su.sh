c060280d961d1e0c141ac37f015fc4380ef71066
Giorgos Logiotatidis <giorgos@mozilla.com>
2014-07-16 14:03:48 +0300

    [fix bug 1039442] Update su.sh script to add real Vouches to superuser.

--- scripts/su.sh
+++ scripts/su.sh
@@ -3,14 +3,18 @@
 echo -n "Enter your BrowserID email: "
 read USEREMAIL
 U=$(cat << _EOF_
-from mozillians.users.models import User, UserProfile
+from mozillians.users.models import User, UserProfile, Vouch
 \nu = User.objects.get(email="$USEREMAIL")
 \nu.is_superuser = True
 \nu.is_staff = True
 \nu.save()
 
 \np = UserProfile.objects.get(user=u)
-\np.is_vouched = True
+\np.vouch(None, description='su vouch 1', autovouch=True)
+\np.vouch(None, description='su vouch 2', autovouch=True)
+\np.vouch(None, description='su vouch 3', autovouch=True)
+\np.vouch(None, description='su vouch 4', autovouch=True)
+\np.vouch(None, description='su vouch 5', autovouch=True)
 \np.save()
 
 _EOF_
