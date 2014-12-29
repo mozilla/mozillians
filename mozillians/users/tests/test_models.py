d4472d8b8482a9eaecdd4ce82ab59e81c8a49653
John Giannelos <johngiannelos@gmail.com>
2014-12-29 12:44:46 +0200

    [Fix bug 1033305] Delete legacy code supporting old vouching system.

--- mozillians/users/tests/test_models.py
+++ mozillians/users/tests/test_models.py
@@ -624,27 +624,6 @@ class VouchTests(TestCase):
         eq_(user.userprofile.vouches_received.all()[0].autovouch, True)
 
     @override_settings(CAN_VOUCH_THRESHOLD=1)
-    @patch('mozillians.users.models.UserProfile._email_now_vouched')
-    @patch('mozillians.users.models.datetime')
-    def test_revouch_legacy_vouch(self, datetime_mock, email_vouched_mock):
-        dt = make_aware(datetime(2012, 01, 01, 00, 10), pytz.UTC)
-        datetime_mock.now.return_value = dt
-        user_1 = UserFactory.create()
-        user_2 = UserFactory.create(vouched=False)
-        ok_(not user_2.userprofile.is_vouched)
-        # Create a legacy vouch
-        Vouch.objects.create(voucher=user_1.userprofile, vouchee=user_2.userprofile)
-
-        user_2.userprofile.vouch(user_1.userprofile, 'Re-Vouching')
-        user_2 = User.objects.get(id=user_2.id)
-        ok_(user_2.userprofile.is_vouched)
-        eq_(user_2.userprofile.vouches_received.all()[0].voucher, user_1.userprofile)
-        eq_(user_2.userprofile.vouches_received.all()[0].date, dt)
-        eq_(user_2.userprofile.vouches_received.all().count(), 1)
-        eq_(user_2.userprofile.vouches_received.all()[0].description, 'Re-Vouching')
-        ok_(email_vouched_mock.called)
-
-    @override_settings(CAN_VOUCH_THRESHOLD=1)
     def test_voucher_public(self):
         voucher = UserFactory.create()
         user = UserFactory.create(vouched=False)
