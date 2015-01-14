f10cd4fb805326713d5a8d458ac9c1a15929773d
Peter Bengtsson <mail@peterbe.com>
2015-01-14 09:26:33 -0800

    correct contribute.json URL

--- mozillians/humans/urls.py
+++ mozillians/humans/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns(
     'mozillians.humans',
     url(r'^humans.txt$', 'views.humans', name='humans'),
-    url(r'^contribute.json/$', 'views.contribute_view', name='contribute-view')
+    url(r'^contribute.json$', 'views.contribute_view', name='contribute-view')
 )
