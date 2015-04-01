e5ad8f0558c64bfaa8710959a03f923d4e351c4f
John Giannelos <johngiannelos@gmail.com>
2015-04-01 17:26:51 +0200

    Upgrade south to 1.0.2

--- vendor-local/lib/python/south/utils/py3.py
+++ vendor-local/lib/python/south/utils/py3.py
@@ -11,11 +11,33 @@ if PY3:
     text_type = str
     raw_input = input
 
+    import io
+    StringIO = io.StringIO
+
 else:
     string_types = basestring,
     text_type = unicode
     raw_input = raw_input
 
+    import cStringIO
+    StringIO = cStringIO.StringIO
+
+
 def with_metaclass(meta, base=object):
     """Create a base class with a metaclass."""
     return meta("NewBase", (base,), {})
+
+
+def _add_doc(func, doc):
+    """Add documentation to a function."""
+    func.__doc__ = doc
+
+if PY3:
+    def iteritems(d, **kw):
+        return iter(d.items(**kw))
+else:
+    def iteritems(d, **kw):
+        return iter(d.iteritems(**kw))
+
+_add_doc(iteritems,
+         "Return an iterator over the (key, value) pairs of a dictionary.")
