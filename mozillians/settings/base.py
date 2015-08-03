bd1d642c2d5282dfeb61c72a6660f71df5f50ec4
Tasos Katsoulas <akatsoulas@gmail.com>
2015-08-03 18:29:24 +0300

    [fix bug 1190535] Enable en-GB locale.

--- mozillians/settings/base.py
+++ mozillians/settings/base.py
@@ -54,7 +54,7 @@ DOMAIN_METHODS = {
 
 # Accepted locales
 LANGUAGE_CODE = 'en-US'
-PROD_LANGUAGES = ('ca', 'cs', 'de', 'en-US', 'es', 'hu', 'fr', 'it', 'ko',
+PROD_LANGUAGES = ('ca', 'cs', 'de', 'en-US', 'en-GB', 'es', 'hu', 'fr', 'it', 'ko',
                   'nl', 'pl', 'pt-BR', 'ro', 'ru', 'sk', 'sl', 'sq', 'sr', 'sv-SE', 'zh-TW',
                   'zh-CN', 'lt', 'ja', 'hsb', 'dsb', 'uk',)
 
