dc149aa8c7d775c39da13f27b5c8497dc9bdb8e9
Tasos Katsoulas <akatsoulas@gmail.com>
2016-03-04 17:21:31 +0200

    Revert "Drop and recreate djcelery table."

--- scripts/update/update.py
+++ scripts/update/update.py
@@ -55,8 +55,6 @@ def update_assets(ctx):
 def database(ctx):
     with ctx.lcd(settings.SRC_DIR):
         ctx.local("python manage.py migrate --noinput")
-        ctx.local("python manage.py migrate djcelery zero")
-        ctx.local("python manage.py migrate djcelery")
 
 
 @task
