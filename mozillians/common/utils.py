72e56d09d4d0ba4f468eabc2d02830ed65f79413
John Giannelos <johngiannelos@gmail.com>
2016-07-26 17:47:29 +0300

    [Fix bug 1289118] Add helper method to check for spam using Akismet API

--- mozillians/common/utils.py
+++ mozillians/common/utils.py
@@ -1,5 +1,10 @@
 from django.conf import settings
 
+import requests
+
+
+AKISMET_URL = 'https://{0}.rest.akismet.com/1.1/comment-check'.format(settings.AKISMET_API_KEY)
+
 
 def absolutify(url):
     """Takes a URL and prepends the SITE_URL"""
@@ -16,3 +21,24 @@ def absolutify(url):
             site_url = ''.join(map(str, (protocol, hostname, ':', port)))
 
     return site_url + url
+
+
+def akismet_spam_check(user_ip, user_agent, **optional):
+    """Checks for spam content against Akismet API."""
+
+    parameters = {
+        'blog': settings.SITE_URL,
+        'user_ip': user_ip,
+        'user_agent': user_agent,
+    }
+
+    parameters.update(optional)
+
+    response = requests.post(AKISMET_URL, data=parameters)
+    response.raise_for_status()
+
+    try:
+        return {'true': True, 'false': False}[response.text]
+    except KeyError:
+        error = response.headers.get('x-akismet-debug-help')
+        raise Exception('Akismet raised an error: {0}'.format(error))
