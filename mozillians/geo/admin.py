555067e571e1963d06f90401318e6635771e4f40
Tasos Katsoulas <akatsoulas@gmail.com>
2016-09-12 15:19:44 +0300

    Enable export in admin for the geo app.

--- mozillians/geo/admin.py
+++ mozillians/geo/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
+from mozillians.common.mixins import MozilliansAdminExportMixin
 from mozillians.geo.models import City, Country, Region
 
 
-class CountryAdmin(admin.ModelAdmin):
+class CountryAdmin(MozilliansAdminExportMixin, admin.ModelAdmin):
     list_display = ('name', 'code')
     readonly_fields = ('name', 'mapbox_id')
     search_fields = ('name', 'code')
@@ -11,7 +12,7 @@ class CountryAdmin(admin.ModelAdmin):
 admin.site.register(Country, CountryAdmin)
 
 
-class RegionAdmin(admin.ModelAdmin):
+class RegionAdmin(MozilliansAdminExportMixin, admin.ModelAdmin):
     list_display = ('name', 'country')
     readonly_fields = ('name', 'country', 'mapbox_id')
     search_fields = ('name', 'country__name', 'country__code')
@@ -19,7 +20,7 @@ class RegionAdmin(admin.ModelAdmin):
 admin.site.register(Region, RegionAdmin)
 
 
-class CityAdmin(admin.ModelAdmin):
+class CityAdmin(MozilliansAdminExportMixin, admin.ModelAdmin):
     list_display = ('name', 'region', 'country', 'lng', 'lat')
     readonly_fields = ('country', 'region', 'name', 'lat', 'lng', 'mapbox_id')
     search_fields = ('name', 'region__name', 'country__name', 'country__code')
