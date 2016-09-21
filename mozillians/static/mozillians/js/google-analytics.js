8a6e39c7a3a133eafd364f9cb5d0fbefc7a0703f
John Giannelos <johngiannelos@gmail.com>
2016-09-21 13:46:22 +0300

    Add missing SRI attributes in mozillians.org js assets

--- mozillians/static/mozillians/js/google-analytics.js
+++ mozillians/static/mozillians/js/google-analytics.js
@@ -2,7 +2,6 @@ var _gaq = _gaq || [];
 var pluginUrl = '//www.google-analytics.com/plugins/ga/inpage_linkid.js';
 var _gaAccountCode = document.documentElement.getAttribute('data-ga-code');
 
-_gaq.push(['_require', 'inpage_linkid', pluginUrl]);
 _gaq.push(['_setAccount', _gaAccountCode]);
 _gaq.push(['_setAllowLinker', true]);
 _gaq.push(['_setAllowAnchor', true]);
@@ -14,9 +13,22 @@ if (_gaAccountCode) {
         var ga = document.createElement('script');
         ga.type = 'text/javascript';
         ga.async = true;
-        ga.src = 'https://ssl.google-analytics.com/ga.js';
+        ga.src = 'static/mozillians/js/libs/ga.js';
+        ga.integrity = 'sha384-qlg7xB42NGK87oFd2d7ZRqeY0XYyrD4wrKw4VhhH8r13OtxJp2UzLymBSaRfN1gK';
+        ga.crossorigin = 'anonymous';
 
         var s = document.getElementsByTagName('script')[0];
         s.parentNode.insertBefore(ga, s);
+
+        var ga_inpage_linkid = document.createElement('script');
+        ga_inpage_linkid.type = 'text/javascript';
+        ga_inpage_linkid.async = true;
+        ga_inpage_linkid.src = 'static/mozillians/js/libs/inpage_linkid.js';
+        ga_inpage_linkid.integrity = 'sha384-sO8ot6WvES5SlygqvT4HdbvgpOsfet1pGROTyNYdMicwjCjOXwU48ITHK8EkNzzp';
+        ga_inpage_linkid.crossorigin = 'anonymous';
+
+        s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(ga_inpage_linkid, s);
+
     })();
 }
