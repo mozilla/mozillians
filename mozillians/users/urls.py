9280bd20cf318891f0c6f349db2feb6ec12ca95a
Tasos Katsoulas <akatsoulas@gmail.com>
2016-10-12 13:12:27 +0300

    Add django-autocomplete-light to API v1.

--- mozillians/users/urls.py
+++ mozillians/users/urls.py
@@ -1,13 +1,15 @@
 from django.conf.urls import patterns, url
 from django.contrib.auth.decorators import login_required
 
-from mozillians.users.views import (BaseProfileAdminAutocomplete, VouchedAutocomplete,
-                                    VoucherAutocomplete)
+from mozillians.users.views import (BaseProfileAdminAutocomplete, UsersAdminAutocomplete,
+                                    VouchedAutocomplete, VoucherAutocomplete)
 
 
 urlpatterns = patterns(
     '',
     # Admin urls for django-autocomplete-light.
+    url('users-autocomplete/$', login_required(UsersAdminAutocomplete.as_view()),
+        name='users-autocomplete'),
     url('vouchee-autocomplete/$', login_required(BaseProfileAdminAutocomplete.as_view()),
         name='vouchee-autocomplete'),
     url('voucher-autocomplete/$', login_required(VoucherAutocomplete.as_view()),
