4b3c2ccedf175807011b41cb1980d1c6eb805684
Yousef Alam <flamingspaz96@googlemail.com>
2017-04-24 13:50:59 +0100

    Add python decouple

--- mozillians/settings/__init__.py
+++ mozillians/settings/__init__.py
@@ -1,19 +1,21 @@
 import sys
+import os
 
-from .base import *  # noqa
+if os.environ.get('MESOS_CLUSTER'):
+    from .base_env_vars import *  # noqa
+else:
+    from .base import *  # noqa
 
-
-try:
-    from .local import *  # noqa
-except ImportError, exc:
-    exc.args = tuple(['%s (did you rename settings/local.py-devdist?)'
-                      % exc.args[0]])
-    raise exc
-
-
-TEST = len(sys.argv) > 1 and sys.argv[1] == 'test'
-if TEST:
     try:
-        from .test import *  # noqa
-    except ImportError:
-        pass
+        from .local import *  # noqa
+    except ImportError, exc:
+        exc.args = tuple(['%s (did you rename settings/local.py-devdist?)'
+                          % exc.args[0]])
+        raise exc
+
+    TEST = len(sys.argv) > 1 and sys.argv[1] == 'test'
+    if TEST:
+        try:
+            from .test import *  # noqa
+        except ImportError:
+            pass
