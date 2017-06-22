8dc6c314c449e258ee98286ea039862b068cfc78
Yousef Alam <flamingspaz96@googlemail.com>
2017-06-22 18:32:24 +0100

    Compress assets at build time

--- bin/run-prod.sh
+++ bin/run-prod.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
 python manage.py migrate --noinput
-python manage.py collectstatic --noinput
-python manage.py compress --engine jinja2 --extension=.html
 gunicorn mozillians.wsgi:application -b 0.0.0.0:${PORT:-8000} --log-file -
