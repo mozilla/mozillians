237fecbaa3e8c4b2693301fb5d214074525389c7
John Giannelos <johngiannelos@gmail.com>
2018-06-04 15:03:41 +0200

    Instantiate AuthZeroManagement client locally.

--- mozillians/phonebook/__init__.py
+++ mozillians/phonebook/__init__.py
@@ -1,11 +1,7 @@
 from django.apps import AppConfig
 
-from mozillians.common.auth0 import MozilliansAuthZeroManagement
-
 
 default_app_config = 'mozillians.phonebook.PhonebookConfig'
-
-AuthZeroManagementApi = MozilliansAuthZeroManagement()
 
 
 class PhonebookConfig(AppConfig):
