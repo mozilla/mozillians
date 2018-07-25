5a9ff0cbfade513b592bf309953bd2f927eb705c
Tasos Katsoulas <akatsoulas@gmail.com>
2018-07-25 15:47:49 +0300

    Add a waffle flag for GraphQL.

--- mozillians/graphql/views.py
+++ mozillians/graphql/views.py
@@ -1,5 +1,7 @@
+from django.http import Http404
 from django.views.decorators.csrf import csrf_exempt
 
+import waffle
 from graphene_django.views import GraphQLView
 
 
@@ -9,4 +11,6 @@ class MozilliansGraphQLView(GraphQLView):
     @csrf_exempt
     def dispatch(self, *args, **kwargs):
         """Override dispatch method to allow the use of multiple decorators."""
+        if not waffle.flag_is_active(self.request, 'enable_graphql'):
+            raise Http404()
         return super(MozilliansGraphQLView, self).dispatch(*args, **kwargs)
