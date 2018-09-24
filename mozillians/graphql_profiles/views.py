8f9d4a74668279f3579028c304793242da9c177c
Tasos Katsoulas <akatsoulas@gmail.com>
2018-09-24 15:00:01 +0300

    Read CSRF token from DOM.

--- mozillians/graphql_profiles/views.py
+++ mozillians/graphql_profiles/views.py
@@ -1,5 +1,4 @@
 from django.http import Http404
-from django.views.decorators.csrf import csrf_exempt
 
 import waffle
 from graphene_django.views import GraphQLView
@@ -8,7 +7,6 @@ from graphene_django.views import GraphQLView
 class MozilliansGraphQLView(GraphQLView):
     """Class Based View to handle GraphQL requests."""
 
-    @csrf_exempt
     def dispatch(self, *args, **kwargs):
         """Override dispatch method to allow the use of multiple decorators."""
         if not waffle.flag_is_active(self.request, 'enable_graphql'):
