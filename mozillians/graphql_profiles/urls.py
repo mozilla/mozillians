c5fceabd26c37ac92c5867aa11f16a030177caa3
Tasos Katsoulas <akatsoulas@gmail.com>
2018-09-24 16:21:09 +0300

    Expose Graphiql only on dev environments.

--- mozillians/graphql_profiles/urls.py
+++ mozillians/graphql_profiles/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import url
 
 from mozillians.graphql_profiles import views
@@ -5,5 +6,5 @@ from mozillians.graphql_profiles import views
 app_name = 'graphql_profiles'
 urlpatterns = [
     # App level graphQL url
-    url(r'^$', views.MozilliansGraphQLView.as_view(graphiql=True), name='graphql_view'),
+    url(r'^$', views.MozilliansGraphQLView.as_view(graphiql=settings.DEV), name='graphql_view'),
 ]
